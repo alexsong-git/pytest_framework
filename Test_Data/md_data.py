6a35bffe9d2f20018996fffe5467cc2a19e97fdb
alex <1372422483@qq.com>
2025-07-02 18:22:13 +0800

    123

--- Test_Data/md_data.py
+++ Test_Data/md_data.py
@@ -1,4 +1,4 @@
 #chromedriver_path = "/Users/alex/.wdm/drivers/chromedriver/mac64/135.0.7049.95/chromedriver-mac-arm64/chromedriver"
 #chromedriver_path = "/Users/alex/.wdm/drivers/chromedriver/mac64/137.0.7151.119/chromedriver-mac-arm64/chromedriver"
-chromedriver_path = "driver/windows/chromedriver-win64/chromedriver.exe"
+chromedriver_path = "driver/mac-x64/chromedriver-mac-x64/chromedriver"
 url_md_dashboard_dev='https://dashboard-dev.seel.com/login'
